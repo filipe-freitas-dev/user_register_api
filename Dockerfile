d23ea3fb3c559f76a67eeedad40324c2deef8bc9
Filipe Freitas <filipe.freitas@filipefreitas.dev>
2025-05-02 10:30:23 -0300

    Dockerfile and docker-compose.yaml added

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM rust:1.86-slim-bullseye
+
+ARG APP_NAME=user_register_api
+
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    libpq-dev \
+    && rm -rf /var/lib/apt/lists/*
+
+COPY . .
+RUN cargo install diesel_cli --no-default-features --features postgres
+RUN cargo build --locked --release
+RUN cp ./target/release/$APP_NAME /bin/server
+
+ENV ROCKET_ADDRESS=0.0.0.0
+CMD ["/bin/server"]
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use repositories::user_repo::UserRepository;
+use rocket::http::Method;
+use rocket_cors::{AllowedOrigins, CorsOptions};
 
 #[macro_use]
 extern crate rocket;
@@ -13,9 +15,22 @@ async fn rocket() -> _ {
     dotenvy::dotenv().ok();
     let pool = database::establish_connection_pool();
 
+    let cors = CorsOptions::default()
+        .allowed_origins(AllowedOrigins::all())
+        .max_age(Some(3600))
+        .allowed_methods(
+            vec![Method::Get, Method::Post, Method::Patch]
+                .into_iter()
+                .map(From::from)
+                .collect(),
+        )
+        .allow_credentials(true);
+
     let user_repo = UserRepository::new(pool);
+
     rocket::build()
         .manage(user_repo)
         .mount("/", services::user_routes::routes())
         .mount("/", services::auth_routes::routes())
+        .attach(cors.to_cors().unwrap())
 }
